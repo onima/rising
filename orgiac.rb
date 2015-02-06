77a17646bbbec4813c7d9aa5aa811ab1da136e63
Alexis Poirier <alexispoirier@live.com>
2015-02-06 18:02:17 +0000

    refactoring orgiac.rb

--- orgiac.rb
+++ orgiac.rb
@@ -27,37 +27,41 @@ helpers do
   end
 
   def response_wrapper
-    game_master_service = GameMasterService.new('orgiac_db', 'orgiac_coll')
-    orgiac_id = session[:orgiac_id]
+    game_master_service = GameMasterService.new(
+      'orgiac_db',
+      'orgiac_coll'
+    )
     game_master =
-      if !orgiac_id
+      if session[:orgiac_id]
+        game_master_hsh = game_master_service.find_by_id(session[:orgiac_id])
+        deserialize(game_master_hsh)
+      else
         game_master = GameMaster.new(GameState.new)
-        game_master.game_state.map_generate
-        game_master.game_state.turn_tracker_generate
-        game_master.game_state.initialize_orgiac_id
+        game_master.game_state.initialize_map_turn_tracker_orgiac_id
         session[:orgiac_id] = game_master.game_state.orgiac_id
         game_master_service.insert(serialize(game_master))
         game_master
-      else
-        gm_hsh = game_master_service.find_by_id(orgiac_id)
-        deserialize(gm_hsh)
       end
-    gm_hsh = game_master_service.find_by_id(session[:orgiac_id])
-    res = yield game_master
-    serialized_gm = serialize(game_master)
-    game_master_service.update(gm_hsh, serialized_gm)
-    res
+    game_master_hsh = game_master_service.find_by_id(session[:orgiac_id])
+    deserialized_game_master = yield game_master
+    game_master_service.update(
+      game_master_hsh,
+      serialize(game_master)
+    )
+    deserialized_game_master
   end
 end
 
 get '/' do
   response_wrapper do |game_master_obj|
-  end
+    @players = game_master_obj.game_state.players
     erb :index
+  end
 end
 
 get '/players_choice' do
   response_wrapper do |game_master_obj|
+    redirect to 'choose_race' if game_master_obj.game_state.players.any?
     if game_master_obj.game_state.raceboard.race_choices.empty?
       game_master_obj.game_state.raceboard.pick_active_races
     end
@@ -116,43 +120,28 @@ get '/play_turn' do
     @map = game_master_obj.game_state.map
     @turn_tracker = game_master_obj.game_state.turn_tracker
     game_master_obj.game_state.players.map.with_index do |player, index|
-      @players = player if turn_tracker.turn_played.length == index
+      @player = player if @turn_tracker.turn_played.length == index
     end
     erb :game
   end
 end
 
 post '/play_turn' do
-  response_wrapper do |game_master_obj|
-  end
-end
 
-#get '/play_turn' do
-#  @players = game_state.players
-#  map
-#  turn_tracker
-#  game_state.players.map.with_index do |player, index|
-#    @player = player if turn_tracker.turn_played.length == index
-#  end
-#   erb :game
-#end
-#
-#post '/play_turn' do
-#
-#  response_wrapper do
-#    region_id = params["land"]
-#    player_string = params["name"]
-#    player = game_state.players.find {|p| p.name == player_string}
-#    @player = player
-#    @players = game_state.players
-#
-#    if region_id
-#    region = map.regions.find {|r| r.id == region_id.to_i}
-#    region.occupied?(@players) ? @player.races[0].troops_number -= region.player_defense : @player.races[0].troops_number -= region.neutral_defense_points && region.player_defense = region.neutral_defense_points
-#     @player.occupied_regions << region
-#    else
-#      turn_tracker.update(@player)
-#    end
-#  end
-#  redirect to 'play_turn'
-#end
+  response_wrapper do |game_master_obj|
+    region_id = params["land"]
+    player_string = params["name"]
+    player = game_master_obj.game_state.players.find {|p| p.name == player_string}
+    @player = player
+    @players = game_master_obj.game_state.players
+
+    if region_id
+    region = game_master_obj.game_state.map.regions.find {|r| r.id == region_id.to_i}
+    region.occupied?(@players) ? @player.races[0].troops_number -= region.player_defense : @player.races[0].troops_number -= region.neutral_defense_points && region.player_defense = region.neutral_defense_points
+     @player.occupied_regions << region
+    else
+      game_master_obj.game_state.turn_tracker.update(@player)
+    end
+  end
+  redirect to '/play_turn'
+end
