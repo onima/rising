87b5290b7dbbc689c37e6a8855853335b8b13e9b
Alexis Poirier <alexispoirier@live.com>
2015-02-23 18:12:49 +0000

    refactoring region presenter

--- presenters/region.rb
+++ presenters/region.rb
@@ -4,7 +4,6 @@ module Presenters
       models = regions.select do |region|
         region.can_be_attacked?(player)
       end
-
       models.map do |model|
         defense_points = model.player_defense || model.neutral_defense_points
         new(model.id, defense_points)
