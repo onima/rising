228054e744cd3687da11fa1ee1869081911c08c9
Alexis Poirier <alexispoirier@live.com>
2015-03-03 18:27:15 +0000

    add capybara test file and change file players_choice.erb in view

---
+++ tests/features/capybara_test.rb
@@ -0,0 +1,24 @@
+ENV['RACK_ENV'] = 'test'
+require 'rising' 
+require 'capybara'
+require 'capybara/dsl'
+require 'minitest/autorun'
+
+class CapybaraTest < Minitest::Test 
+    include Capybara::DSL
+
+    def setup
+      Capybara.app = Sinatra::Application.new
+    end
+    
+    def test_if_application_works
+      visit '/'
+      page.click_on('new_game')
+      assert_equal '/players_choice', page.current_path
+      
+      #User must use comma to create players
+      page.fill_in 'players', :with => 'Alice Bob John'
+      page.click_on('validate_players')
+      assert_equal '/choose_race', page.current_path
+    end
+end
