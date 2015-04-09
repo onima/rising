1fd7a7f9fd9a23ea78ad767471a96b487e46c136
Alexis Poirier <alexispoirier@live.com>
2015-04-09 17:39:33 +0100

    Add color to hexagons

--- public/js/main.js
+++ public/js/main.js
@@ -2,10 +2,13 @@
 
   var map                       = { width: 5, height: 6 };
   var r                         = 50;
-  var hexagonSemiVerticalHeight = Math.sqrt(3) * r / 2;
   var hexagonArray              = [];
   var svg                       = SVG('drawing').size(600, 700);
   var xhr                       = new XMLHttpRequest();
+
+  var capitalize = function(str) {
+        return str.charAt(0).toUpperCase() + str.slice(1);
+  };
 
   xhr.onreadystatechange = alertContents;
   xhr.open('GET', 'http://localhost:9292/regions_hsh');
@@ -15,17 +18,18 @@
     if (xhr.readyState === 4) {
       if (xhr.status === 200) {
         regions_object = JSON.parse(xhr.responseText);
+        createMap(regions_object);
       } else {
         alert('There was a problem with the request.');
       }
     }
   }
 
-  var createMap = function createMap() {
+  var createMap = function createMap(regions_object) {
 
   var findHexagonCoordinates = function findHexagonCoordinates(xCentre, yCentre) {
     var hexagonCoordinates = [];
-    for (var k = 0; k < 6; k++) {
+    for (var k = 0; k < 7; k++) {
       var coordinates = {
         x: r * Math.cos(2*Math.PI*k/6) + xCentre,
         y: r * Math.sin(2*Math.PI*k/6) + yCentre
@@ -43,18 +47,16 @@
 
   var drawMap = function drawMap(map) {
 
-    for(var i = 0; i < map.width; i++) {
-      for(var j = 0; j < map.height; j++) {
+    for(var i = 1; i < map.width + 1; i++) {
+      for(var j = 1; j < map.height + 1; j++) {
         var array_i_j = f(i,j);
-        var coordinates = findHexagonCoordinates(array_i_j[0] + 50, array_i_j[1] + 50);
-        hexagonArray.push(svg.polyline([coordinates]));
+        var coordinates = findHexagonCoordinates(array_i_j[0], array_i_j[1]);
+        hexagonArray.push(svg.polyline([coordinates]).attr({ fill: regions_object[i + ',' + j].color, 'fill-opacity': 0.6, stroke: '#000', text: 'hello' }));
       }
     }
     return hexagonArray;
   };
   drawMap(map);
   };
-
-  createMap();
 
 }) ();
