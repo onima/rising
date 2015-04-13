4df70ae5dcbda500917f43d6e1fb9dab06ee957c
Alexis Poirier <alexispoirier@live.com>
2015-04-13 18:22:36 +0100

    Add new feature when hover map and refactor code in main.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,78 +1,69 @@
 (function() {
 
-  var map                       = { width: 5, height: 6 };
-  var r                         = 50;
-  var svg                       = SVG('drawing').size(600, 700);
-  var xhr                       = new XMLHttpRequest();
+  var xhr = new XMLHttpRequest();
 
-  var capitalize = function(str) {
-        return str.charAt(0).toUpperCase() + str.slice(1);
-  };
-
-  var lpad = function(string, padString, length) {
-      var str = string;
-          while (str.length < length)
-          str = padString + str;
-    return str;
-  };
-
-  var mouseOver = function() {
-    this.fill({ color: '#f06' });
-  };
-
-  xhr.onreadystatechange = alertContents;
-  xhr.open('GET', 'http://localhost:9292/regions_hsh');
-  xhr.send();
-
-  xhr.onreadystatechange = alertContents;
-  xhr.open('GET', 'http://localhost:9292/conquerable_regions');
-  xhr.send();
-
-  function alertContents() {
+  xhr.onreadystatechange = function() {
     if (xhr.readyState === 4) {
-      if (xhr.status === 200) {
+      if (xhr.status === 200 || xhr.status === 0) {
         regions_object = JSON.parse(xhr.responseText);
         createMap(regions_object);
       } else {
         alert('There was a problem with the request.');
       }
     }
-  }
+  }; 
+  xhr.open("GET", 'http://localhost:9292/regions_hsh');
+  xhr.send(null);
 
   var createMap = function createMap(regions_object) {
 
-  var findHexagonCoordinates = function findHexagonCoordinates(xCentre, yCentre) {
-    var hexagonCoordinates = [];
-    for (var k = 0; k < 7; k++) {
-      var coordinates = {
-        x: r * Math.cos(2*Math.PI*k/6) + xCentre,
-        y: r * Math.sin(2*Math.PI*k/6) + yCentre
-      };
-      hexagonCoordinates.push(coordinates.x, coordinates.y);
-    }
-   return hexagonCoordinates;
-  };
+    var svg                       = SVG('drawing').size(600, 700);
+    var map                       = svg.group().addClass('map');
+    var map_dimensions            = { width: 5, height: 6 };
+    var r                         = 50;
 
-  var f = function f(i,j) {
-    var x = 1.5 * i * r;
-    var y = i % 2 === 0 ? Math.sqrt(3) * j * r : Math.sqrt(3) * j * r + ((Math.sqrt(3) / 2) * r);
-    return [x, y];
-  };
-
-  var drawMap = function drawMap(map) {
-
-    for(var i = 1; i < map.width + 1; i++) {
-      for(var j = 1; j < map.height + 1; j++) {
-        var array_i_j = f(i,j);
-        var coordinates = findHexagonCoordinates(array_i_j[0], array_i_j[1]);
-        var region_object = regions_object[i + ',' + j];
-        var defense_points_str = svg.text(region_object.conquest_points + '').attr({ x: coordinates[6] + 45, y: coordinates[7] - 15 });
-        var hexagon = svg.polyline([coordinates]).attr({ fill: regions_object[i + ',' + j].color, 'fill-opacity': 0.5, stroke: '#000' });
-        //hexagon.on('mouseover', mouseOver);
+    var findHexagonCoordinates = function findHexagonCoordinates(xCentre, yCentre) {
+      var hexagonCoordinates = [];
+      for (var k = 0; k < 7; k++) {
+        var coordinates = {
+          x: r * Math.cos(2*Math.PI*k/6) + xCentre,
+          y: r * Math.sin(2*Math.PI*k/6) + yCentre
+        };
+        hexagonCoordinates.push(coordinates.x, coordinates.y);
       }
-    }
-  };
-  drawMap(map);
+      return hexagonCoordinates;
+    };
+
+    var f = function f(i,j) {
+      var x = 1.5 * i * r;
+      var y = i % 2 === 0 ? Math.sqrt(3) * j * r : Math.sqrt(3) * j * r + ((Math.sqrt(3) / 2) * r);
+      return [x, y];
+    };
+
+    var drawMap = function drawMap(map_dimensions) {
+
+      for(var i = 1; i < map_dimensions.width + 1; i++) {
+        for(var j = 1; j < map_dimensions.height + 1; j++) {
+          var i_j_array = f(i,j);
+          var coordinates = findHexagonCoordinates(i_j_array[0], i_j_array[1]);
+          var region_object = regions_object[i + ',' + j];
+          var hexagon = svg.polyline([coordinates]).attr( {
+            fill: regions_object[i + ',' + j].color,
+            stroke: '#000'
+          } );
+          hexagon.addClass('hexagon');
+          if (region_object.attackable) {
+            hexagon.addClass('attackable');
+          }
+          map.add(hexagon);
+          var defense_points_text = svg.text(region_object.conquest_points + '').attr( {
+            x: coordinates[6] + 45,
+            y: coordinates[7] - 15
+          } );
+        }
+      }
+    };
+    drawMap(map_dimensions);
   };
 
 }) ();
